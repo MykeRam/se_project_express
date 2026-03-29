d16472bb1f1de48f7af925f81d32fd2a1f64fa76
Michael Ramirez <45132037+MykeRam@users.noreply.github.com>
2026-03-28 21:08:41 -0400

    Add user and clothing item models

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -1,1 +1,39 @@
-module.exports = {};
+const mongoose = require("mongoose");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    type: String,
+    required: true,
+    enum: ["hot", "warm", "cold"],
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("clothingItem", clothingItemSchema);
