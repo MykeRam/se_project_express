689703f45f9dd60b15b31ec63b5010b39e062fde
Michael Ramirez <45132037+MykeRam@users.noreply.github.com>
2026-04-12 22:58:43 -0400

    Add user auth fields and password hashing

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,13 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const {
   BAD_REQUEST,
+  CONFLICT,
   NOT_FOUND,
   INTERNAL_SERVER_ERROR,
 } = require("../utils/errors");
+
+const SALT_ROUNDS = 10;
 
 const getUsers = (req, res) => {
   User.find({})
@@ -37,15 +41,35 @@ const getUser = (req, res) => {
 };
 
 const createUser = (req, res) => {
-  const { name, avatar } = req.body;
+  const {
+    name, avatar, email, password,
+  } = req.body;
 
-  User.create({ name, avatar })
-    .then((user) => res.status(201).send(user))
+  bcrypt
+    .hash(password, SALT_ROUNDS)
+    .then((hash) => User.create({
+      name,
+      avatar,
+      email,
+      password: hash,
+    }))
+    .then((user) => res.status(201).send({
+      _id: user._id,
+      name: user.name,
+      avatar: user.avatar,
+      email: user.email,
+    }))
     .catch((err) => {
       if (err.name === "ValidationError") {
         return res
           .status(BAD_REQUEST)
           .send({ message: "Invalid data passed to create a user" });
+      }
+
+      if (err.code === 11000) {
+        return res
+          .status(CONFLICT)
+          .send({ message: "A user with that email already exists" });
       }
 
       return res
--- utils/errors.js
+++ utils/errors.js
@@ -1,9 +1,11 @@
 const BAD_REQUEST = 400;
+const CONFLICT = 409;
 const NOT_FOUND = 404;
 const INTERNAL_SERVER_ERROR = 500;
 
 module.exports = {
   BAD_REQUEST,
+  CONFLICT,
   NOT_FOUND,
   INTERNAL_SERVER_ERROR,
 };
