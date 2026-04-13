940db7236c83cd1ce6b78f09a866c4e90581866b
Michael Ramirez <45132037+MykeRam@users.noreply.github.com>
2026-04-13 01:43:03 -0400

    Enable CORS in app setup

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
@@ -7,6 +8,7 @@ const { PORT = 3001 } = process.env;
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
