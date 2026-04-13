9af488ab00751c0ec65a849435ca01a561494925
Michael Ramirez <45132037+MykeRam@users.noreply.github.com>
2026-04-12 23:05:28 -0400

    Add signin and signup routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const clothingItemRouter = require("./clothingItems");
 const userRouter = require("./users");
+const { login, createUser } = require("../controllers/users");
 const { NOT_FOUND } = require("../utils/errors");
 
+router.post("/signin", login);
+router.post("/signup", createUser);
 router.use("/items", clothingItemRouter);
 router.use("/users", userRouter);
 router.use((req, res) => {
