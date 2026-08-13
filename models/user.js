324484e5abd13a520c9be41860b95762e411ea1d
Michael Ramirez <45132037+MykeRam@users.noreply.github.com>
2026-08-12 22:04:12 -0400

    Support unique emails with legacy users

--- models/user.js
+++ models/user.js
@@ -24,6 +24,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+    sparse: true,
     validate: {
       validator(value) {
         return validator.isEmail(value);
