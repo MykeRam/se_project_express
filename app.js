bedcace4dab7937edaf1a490130c315a16f3f1db
Michael Ramirez <45132037+MykeRam@users.noreply.github.com>
2026-08-15 14:20:47 -0400

    test: add PM2 crash test route

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ mongoose.connect(MONGO_URL);
 app.use(requestLogger);
 app.use(cors());
 app.use(express.json());
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use(routes);
 app.use(errorLogger);
 app.use(errors());
